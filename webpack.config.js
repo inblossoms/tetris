44f94dab40c4a0d1acbb471fdac2578b262b580a
inblossoms <L13994352192@163.com>
2023-10-06 17:14:12 +0800

    chore: webpack.config about performance

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const { resolve } = require("path"),
+  webpack = require("webpack"),
   HtmlWebpackPlugin = require("html-webpack-plugin"),
   { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
@@ -15,11 +16,32 @@ module.exports = {
       template: "./public/index.html",
     }),
     new CleanWebpackPlugin(),
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 5,
+    }),
   ],
+  performance: {
+    hints: "warning",
+    maxEntrypointSize: 2048000,
+    maxAssetSize: 1024000,
+    assetFilter: function (assetFilename) {
+      return assetFilename.endsWith(".js");
+    },
+  },
   module: {
     rules: [{ test: /.ts$/, loader: "ts-loader" }],
   },
   resolve: {
     extensions: [".ts", ".js"],
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  optimization: {
+    minimize: true,
+    splitChunks: {
+      chunks: "all",
+      minSize: 102400,
+    },
   },
 };
